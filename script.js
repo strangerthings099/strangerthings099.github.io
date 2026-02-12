ac15eff7449d178a7c2e4e07da0baf15c2ec80ab
strangerthings <87391027+strangerthings099@users.noreply.github.com>
2026-02-12 17:51:38 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,3 +5,33 @@ ScrollSmoother.create({
     effects: true
 })
 
+// Animação da seção hero
+
+gsap.from(".hero", {
+    opacity: 0,
+    duration: 1
+})
+
+gsap.from("picture:nth-child(2)", {
+    y: 60,
+    duration: 1,
+})
+
+gsap.from("picture:nth-child(1)", {
+    y: -60,
+    duration: 1,
+})
+
+// Animação da seção cards
+
+gsap.from(".card", {
+    opacity: 0,
+    duration: 1,
+    y: 30,
+    filter: "blur(20px)",
+    stagger: .3,
+    scrollTrigger: {
+        trigger: ".cards",
+        start: "0% 70%",
+    }
+})
